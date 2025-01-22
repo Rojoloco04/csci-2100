665ed3f380b0f9ee6cb49d2713eb931794285dd2
Rojoloco04 <parrackjack@gmail.com>
2025-01-22 14:56:04 -0600

    Update data_types

--- data_types.cpp
+++ data_types.cpp
@@ -1,1 +1,68 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+// Part 1
+/* int main() {
+    string fruit;
+    
+    cout << "What is your favorite fruit?" << endl;
+    cin >> fruit;
+
+    cout << "I like " << fruit << "s too!" << endl;
+    return 0;
+} */
+
+// Part 2
+/* int main() {
+    int age;
+    
+    cout << "How old are you?" << endl;
+    cin >> age;
+    
+    age += 25;
+    cout << "In 25 years you will be " << age << " years old." << endl;
+    return 0;
+} */
+
+// Part 3
+/* int main () {
+    double celsius, fahrenheit;
+    
+    cout << "Enter a temperature in Celsius: ";
+    cin >> celsius;
+    
+    fahrenheit = (9.0/5 * celsius) + 32;
+    cout << celsius << " Celsius is equal to " << fahrenheit << " Fahrenheit" << endl;
+    return 0;
+} */
+
+// Part 4
+/* int main () {
+    double beats, seconds;
+    int bpm;
+
+    cout << "How many beats were observed? ";
+    cin >> beats;
+    cout << "How many seconds were observed? ";
+    cin >> seconds;
+
+    bpm = beats / seconds * 60;
+    cout << "The pulse is " << bpm << " beats per minute." << endl;
+    return 0;
+} */
+
+// Part 5
+/* int main () {
+    int m, n, sum;
+
+    cout << "Enter the first number: ";
+    cin >> m;
+    cout << "Enter the second number: ";
+    cin >> n;
+
+    sum = (n - m + 1) * (m + n)/2;
+    cout << "The sum of the numbers from " << m << " to " << n << " is " << sum << "." << endl;
+    return 0;
+} */
