1d3b1f4b52d4c139a2bb3b6b2be47b841122afde
Rojoloco04 <parrackjack@gmail.com>
2025-01-23 12:18:53 -0600

    Update loops

--- loops.cpp
+++ loops.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <string>
+#include <random>
+#include <chrono>
 
 using namespace std;
 
 int main() {
-    
+    // generate random number
+    std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<int> distribution(1,100);
+    int number = distribution(generator);
+
+    // initalize variables
+    int guess{0}, tries{0};
+    bool correct{false};
+
+    cout << "I've picked a random number from 1 to 100, try to guess it." << endl;
+
+    while (!correct) {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        tries++;
+
+        if (guess > number)
+            cout << "Too high" << endl;
+        else if (guess < number)
+            cout << "Too low" << endl;
+        else if (guess == number) {
+            cout << "You found it in " << tries << " guesses!" << endl;
+            correct = true; }
+    }
 }
